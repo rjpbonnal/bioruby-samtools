422ea8ee6f74d8dd03496003e63989408c539a79
Raoul J.P. Bonnal <ilpuccio.febo@gmail.com>
2011-06-01 21:17:56 +0200

    forget to copy the compiled library

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -36,14 +36,16 @@ end
 task :compile do
   sh "tar xvfj #{SamToolsFile}"
   cd("samtools-#{Version}") do
-    cp("libbam.a","#{path_external}")
+
     case Config::CONFIG['host_os']
       when /linux/
         sh "CFLAGS='-g -Wall -O2 -fPIC' make -e"
+        cp("libbam.a","#{path_external}")
         sh "CFLAGS='-g -Wall -O2 -fPIC' make -e libbam.so.1-local"
         cp("libbam.so.1","#{path_external}")
       when /darwin/
         sh "make"
+        cp("libbam.a","#{path_external}")
         sh "make libbam.1.dylib-local"
         cp("libbam.1.dylib","#{path_external}")      
       when /mswin|mingw/ then raise NotImplementedError, "BWA library is not available for Windows platform"  
