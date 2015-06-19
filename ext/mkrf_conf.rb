ae8fe81d5db7cfda44ba0ceb4b22dbb455acd7e5
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-06-19 15:28:23 +0100

    Removed the dylib dependencies, so we don't need to check the OS, since we are not using FFI anymore

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -39,24 +39,8 @@ task :compile do
     sh "patch < ../Makefile-bioruby.patch"
     # This patch replace CURSES lib with NCURSES which it is the only one available in OpenSUSE
     sh "patch < ../Makefile-suse.patch"
-    case Config::CONFIG['host_os']
-      when /linux/
-        #sh "CFLAGS='-g -Wall -O2 -fPIC' make -e"
-        sh "make"
-        cp("libbam.a","#{path_external}")
-        #sh "CFLAGS='-g -Wall -O2 -fPIC' make -e libbam.so.1-local"
-        sh "make libbam.so.1-local"
-        cp("samtools", "#{path_external}")
-        cp("libbam.so.1","#{path_external}")
-      when /darwin/
-        sh "make"
-        cp("libbam.a","#{path_external}")
-        sh "make libbam.1.dylib-local"
-        cp("libbam.1.dylib","#{path_external}")
-        sh "make"
-        cp('samtools', "#{path_external}")      
-      when /mswin|mingw/ then raise NotImplementedError, "BWA library is not available for Windows platform"  
-    end #case
+    sh "make"
+    cp('samtools', "#{path_external}")          
   end #cd
   cd("samtools-#{Version}/bcftools") do
     sh "make"
