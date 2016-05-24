a99652d9bfd9230a0ed68ee162de53e4960a185c
Ricardo H. Ramírez-Gonzalez <homo.nec.loco@gmail.com>
2016-05-24 23:30:30 +0100

    Updated samtools and bcftools to version 1.3.1 #44 #59

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -12,41 +12,50 @@ version = File.open(File.join(path_external,"VERSION"),'r')
 Version = version.read
 version.close
 
-url = "http://sourceforge.net/projects/samtools/files/samtools/#{Version}/samtools-#{Version}.tar.bz2/download"
+#url = "http://sourceforge.net/projects/samtools/files/samtools/#{Version}/samtools-#{Version}.tar.bz2/download"
+url="https://github.com/samtools/samtools/releases/download/#{Version}/samtools-#{Version}.tar.bz2"
 SamToolsFile = "samtools-#{Version}.tar.bz2"
+url_bcftools="https://github.com/samtools/bcftools/releases/download/#{Version}/bcftools-#{Version}.tar.bz2"
+BcfToolsFile = "bcftools-#{Version}.tar.bz2"
 
 File.open(File.join(path,"Rakefile"),"w") do |rakefile|
 rakefile.write <<-RAKE
 require 'rbconfig'
 require 'open-uri'
-require 'open_uri_redirections'
+#require 'open_uri_redirections'
 require 'fileutils'
 include FileUtils::Verbose
 require 'rake/clean'
 
 URL = "#{url}"
-
+URL_bcf = "#{url_bcftools}"
 task :download do
-  open(URL, :allow_redirections => :all) do |uri|
+  open(URL) do |uri|
     File.open("#{SamToolsFile}",'wb') do |fout|
       fout.write(uri.read)
     end #fout
   end #uri
+
+  open(URL_bcf) do |uri|
+    File.open("#{BcfToolsFile}",'wb') do |fout|
+      fout.write(uri.read)
+    end #fout
+  end #uri
+
 end
 
 task :compile do
   sh "tar xvfj #{SamToolsFile}"
   cd("samtools-#{Version}") do
-    sh "patch < ../Makefile-bioruby.patch"
-    # This patch replace CURSES lib with NCURSES which it is the only one available in OpenSUSE
-    sh "patch < ../Makefile-suse.patch"
     sh "make"
     cp('samtools', "#{path_external}")
   end #cd
-  cd("samtools-#{Version}/bcftools") do
+
+  sh "tar xvfj #{BcfToolsFile}"
+  cd("bcftools-#{Version}") do
     sh "make"
     cp('bcftools', "#{path_external}")
-  end
+  end #cd
 end
 
 task :clean do
@@ -55,6 +64,8 @@ task :clean do
   end
   rm("#{SamToolsFile}")
   rm_rf("samtools-#{Version}")
+  rm("#{BcfToolsFile}")
+  rm_rf("bcftools-#{Version}")
 end
 
 task :default => [:download, :compile, :clean]
